7162d34d3772009529739ff1f23a7751aa1e9001
YelChristensen <yelena.christensen@gmail.com>
2020-10-14 21:19:55 +0100

    adding a grid to the content page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,31 +1,39 @@
 import React from "react";
-import { Box, Grid, Link, Typography } from "@material-ui/core";
+import Search from "./Search";
+import { Box, Grid, Link } from "@material-ui/core";
+
 export default function Content() {
   return (
-    <Grid container direction="column">
-      <Grid item xs={12}>
-        <Box p={2} textAlign="center">
-          Find a nanny the easy way
-        </Box>
+    <Grid container>
+      <Grid item xs={1} sm={2} />
+      <Grid item container xs={10} sm={8} direction="column">
+        <Grid item>
+          <Box p={2} textAlign="center">
+            Find a perfect nanny the easy way
+          </Box>
+        </Grid>
+        <Grid>
+          <Search />
+        </Grid>
+        <Grid>
+          A nanny provides childcare for one or more children in the parent's
+          own home. Nannies can sometimes live-in with the family. Many families
+          have a full-time nanny but it is also popular to employ a part-time
+          nanny or after school live-out nanny. Nannies who provide care for
+          newly born babies during the night are known as night nannies.
+        </Grid>
+        <Grid>
+          <Link>Choosing the right Nanny</Link>
+        </Grid>
+        <Grid>
+          <Link>Paying your Nanny</Link>
+        </Grid>
+        <Grid>
+          For local Nanny results enter your full postcode in the search above
+          or try our Advanced Search feature.
+        </Grid>
       </Grid>
-      <Grid item>Search menu</Grid>
-      <Grid item>
-        A nanny provides childcare for one or more children in the parent's own
-        home. Nannies can sometimes live-in with the family. Many families have
-        a full-time nanny but it is also popular to employ a part-time nanny or
-        after school live-out nanny. Nannies who provide care for newly born
-        babies during the night are known as night nannies.
-      </Grid>
-      <Grid item>
-        <Link>Choosing the right Nanny</Link>
-      </Grid>
-      <Grid item>
-        <Link>Paying your Nanny</Link>
-      </Grid>
-      <Grid item>
-        For local Nanny results enter your full postcode in the search above or
-        try our Advanced Search feature.
-      </Grid>
+      <Grid item xs={1} sm={2} md={3} />
     </Grid>
   );
 }
