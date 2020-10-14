736a1c74efa4cb0b570f7957e201069910d0d705
YelChristensen <yelena.christensen@gmail.com>
2020-10-14 22:21:32 +0100

    working on search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,57 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import Dropdown from "./Dropdown";
+import { Box, Button, Grid } from "@material-ui/core";
 
 export default function Search() {
-  return <Grid>Search function</Grid>;
+  return (
+    <Grid container spacing={2} alignItems="flex-end">
+      <Grid
+        item
+        container
+        xs={12}
+        md={6}
+        lg={3}
+        direction="column"
+        alignContent="center"
+      >
+        <Box textAlign="center">Location:</Box>
+        <Dropdown />
+      </Grid>
+      <Grid
+        item
+        container
+        xs={12}
+        md={6}
+        lg={3}
+        direction="column"
+        alignContent="center"
+      >
+        <Box textAlign="center">Child's Age:</Box>
+        <Dropdown />
+      </Grid>
+      <Grid
+        item
+        container
+        xs={12}
+        md={6}
+        lg={3}
+        direction="column"
+        alignContent="center"
+      >
+        <Box textAlign="center">Filter:</Box>
+        <Dropdown />
+      </Grid>
+      <Grid
+        item
+        container
+        xs={12}
+        md={6}
+        lg={3}
+        direction="column"
+        alignContent="center"
+      >
+        <Button>Search</Button>
+      </Grid>
+    </Grid>
+  );
 }
