2243270349d7d9c685459eb6e6b1b634c3491492
YelChristensen <yelena.christensen@gmail.com>
2020-11-26 14:07:39 +0000

    modifying the card layout

--- src/components/Nanny.js
+++ src/components/Nanny.js
@@ -6,28 +6,35 @@ import {
   CardHeader,
   CardMedia,
   CardContent,
+  Grid,
 } from "@material-ui/core";
 import PhoneTwoToneIcon from "@material-ui/icons/PhoneTwoTone";
 import EmailTwoToneIcon from "@material-ui/icons/EmailTwoTone";
 
 const Nanny = (props) => {
-  console.log(props.nanny);
+  console.log(props.nanny.photo);
   return (
     <Card>
-      <CardHeader
-        title={`${props.nanny.first_name}  ${props.nanny.last_name}`}
-        subheader={props.nanny.city}
-      />
-      <CardMedia image={props.nanny.photo} />
-      <CardContent>{props.nanny.bio}</CardContent>
-      <Button>
-        <PhoneTwoToneIcon />
-        {props.nanny.tel}
-      </Button>
-      <Button>
-        <EmailTwoToneIcon />
-        {props.nanny.email}
-      </Button>
+      <Grid container>
+        <Grid item xs={6}>
+          <CardHeader
+            title={`${props.nanny.first_name}  ${props.nanny.last_name}`}
+            subheader={props.nanny.city}
+          />
+          <CardContent>{props.nanny.bio}</CardContent>
+          <Button>
+            <PhoneTwoToneIcon />
+            {props.nanny.tel}
+          </Button>
+          <Button>
+            <EmailTwoToneIcon />
+            {props.nanny.email}
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <CardMedia component="img" image={props.nanny.photo} />
+        </Grid>
+      </Grid>
     </Card>
   );
 };
